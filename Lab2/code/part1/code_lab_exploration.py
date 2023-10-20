a4c8dfdcd2d118319be8de1f9f19e8358637c7a1
Balthazar Neveu <balthazarneveu@gmail.com>
2023-10-20 18:16:47 +0200

    empty code labs

---
+++ Lab2/code/part1/code_lab_exploration.py
@@ -0,0 +1,49 @@
+"""
+Graph Mining - ALTEGRAD - Oct 2023
+"""
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+############## Task 1
+
+##################
+# your code here #
+##################
+
+
+
+############## Task 2
+
+##################
+# your code here #
+##################
+
+
+
+############## Task 3
+# Degree
+degree_sequence = [G.degree(node) for node in G.nodes()]
+
+##################
+# your code here #
+##################
+
+
+
+############## Task 4
+
+##################
+# your code here #
+##################
+
+
+
+
+############## Task 5
+
+##################
+# your code here #
+##################
---
+++ Lab2/code/part2/code_lab_community_detection.py
@@ -0,0 +1,66 @@
+"""
+Graph Mining - ALTEGRAD - Oct 2023
+"""
+
+import networkx as nx
+import numpy as np
+from scipy.sparse.linalg import eigs
+from scipy.sparse import diags, eye
+from random import randint
+from sklearn.cluster import KMeans
+
+
+
+############## Task 6
+# Perform spectral clustering to partition graph G into k clusters
+def spectral_clustering(G, k):
+    
+    ##################
+    # your code here #
+    ##################
+    
+
+    
+    return clustering
+
+
+
+
+
+############## Task 7
+
+##################
+# your code here #
+##################
+
+
+
+
+
+############## Task 8
+# Compute modularity value from graph G based on clustering
+def modularity(G, clustering):
+    
+    ##################
+    # your code here #
+    ##################
+    
+
+    
+    
+    return modularity
+
+
+
+############## Task 9
+
+##################
+# your code here #
+##################
+
+
+
+
+
+
+
