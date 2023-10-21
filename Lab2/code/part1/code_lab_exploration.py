f060a958f378999b1d397709ce95f017dafa1aec
Balthazar Neveu <balthazarneveu@gmail.com>
2023-10-21 13:12:36 +0200

    task 1 load & display graph stats

--- Lab2/code/part1/code_lab_exploration.py
+++ Lab2/code/part1/code_lab_exploration.py
@@ -5,15 +5,36 @@ Graph Mining - ALTEGRAD - Oct 2023
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
+from pathlib import Path
+import pprint
 
 
 ############## Task 1
+def load_graph(edge_path: Path) -> nx.Graph:
+    graph = nx.read_edgelist(edges_file)
+    return graph
 
-##################
-# your code here #
-##################
+def get_stats(graph: nx.Graph):
+    stats = {
+        "#nodes": len(graph.nodes),
+        "#edges": len(graph.edges)
+    }
+    return stats
 
 
+def task_1(path: Path):
+    graph = load_graph(path)
+    stats = get_stats(graph)
+    # print(stats)
+    pprint.pprint(stats, width=10)
+
+if __name__ == "__main__":
+    
+    # DATASET_FOLDER = Path("code/datasets")
+    dataset_folder = Path(__file__).parent/".."/"datasets"
+    edges_file = dataset_folder/"CA-HepTh.txt"
+    task_1(edges_file)
+    
 
 ############## Task 2
 
@@ -25,7 +46,7 @@ import numpy as np
 
 ############## Task 3
 # Degree
-degree_sequence = [G.degree(node) for node in G.nodes()]
+# degree_sequence = [G.degree(node) for node in G.nodes()]
 
 ##################
 # your code here #
