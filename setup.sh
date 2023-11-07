5233bdcec64fa497717bd8fa2762588095171bf5
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-07 21:35:30 +0000

    setup environment (conda for llm)

---
+++ setup.sh
@@ -0,0 +1,16 @@
+conda create -n llm python=3.11
+conda activate llm
+pip install sentencepiece
+cd ~
+git clone https://github.com/hadi-abdine/fairseq
+cd fairseq
+pip install -e .
+cd ~
+git clone https://github.com/huggingface/transformers.git
+cd transformers
+pip install -e .
+cd ~
+pip install datasets
+pip install evaluate
+pip install sentencepiece
+pip install tensorboardX
