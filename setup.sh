a1d4f9029e94e8d5bdf7bff0b1255000620c9b3b
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-10 16:31:32 +0000

    update setup - use python 3.9

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-conda create -n llm python=3.11
-conda activate llm
+conda create -n llm_altegrad python=3.9
+conda activate llm_altegrad
 pip install sentencepiece
 cd ~
-git clone https://github.com/hadi-abdine/fairseq
+# git clone https://github.com/hadi-abdine/fairseq
 cd fairseq
 pip install -e .
 cd ~
-git clone https://github.com/huggingface/transformers.git
+# git clone https://github.com/huggingface/transformers.git
 cd transformers
 pip install -e .
 cd ~
