8be4fb763f23c8de22fdde1bebfd5baf50eb0730
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-10 17:14:49 +0000

    remote setup

---
+++ remote_setup.sh
@@ -0,0 +1,16 @@
+git clone https://github.com/balthazarneveu/gpu_cloud.git
+cd gpu_cloud
+. first.sh
+. second.sh
+cat ~/.ssh/id_ed25519.pub
+
+### ADD SSH KEY TO GITHUB
+
+
+
+# INSTALL LAB ENVIRONMENT
+git clone git@github.com:balthazarneveu/MVA23_ALTEGRAD.git
+cd ~
+. MVA23_ALTEGRAD/setup.sh
+cd ~/MVA23_ALTEGRAD/Lab4
+. download_models.sh
