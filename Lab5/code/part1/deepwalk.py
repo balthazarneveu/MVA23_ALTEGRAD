6a8c46e20a02b5629b381221e738a25319b89f4b
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-14 14:12:51 +0100

    initiate lab  5 (instructions, empty code)

---
+++ Lab5/code/part1/deepwalk.py
@@ -0,0 +1,45 @@
+"""
+Deep Learning on Graphs - ALTEGRAD - Nov 2023
+"""
+
+import numpy as np
+import networkx as nx
+from random import randint
+from gensim.models import Word2Vec
+
+
+############## Task 1
+# Simulates a random walk of length "walk_length" starting from node "node"
+def random_walk(G, node, walk_length):
+
+    ##################
+    # your code here #
+    ##################
+    
+	walk = [str(node) for node in walk]
+	return walk
+
+
+############## Task 2
+# Runs "num_walks" random walks from each node
+def generate_walks(G, num_walks, walk_length):
+    walks = []
+    
+    ##################
+    # your code here #
+    ##################
+
+    return permuted_walks.tolist()
+
+
+# Simulates walks and uses the Skipgram model to learn node representations
+def deepwalk(G, num_walks, walk_length, n_dim):
+    print("Generating walks")
+    walks = generate_walks(G, num_walks, walk_length)
+
+    print("Training word2vec")
+    model = Word2Vec(vector_size=n_dim, window=8, min_count=0, sg=1, workers=8, hs=1)
+    model.build_vocab(walks)
+    model.train(walks, total_examples=model.corpus_count, epochs=5)
+
+    return model
