2f76a11ce773f522b61750c57c50c6e7f9d7fe89
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-15 00:05:20 +0100

    update random walks for faster generation

--- Lab5/code/part1/deepwalk.py
+++ Lab5/code/part1/deepwalk.py
@@ -1,35 +1,42 @@
 """
 Deep Learning on Graphs - ALTEGRAD - Nov 2023
 """
-
 import numpy as np
 import networkx as nx
 from random import randint
 from gensim.models import Word2Vec
-
+from typing import Union, List
 
 ############## Task 1
 # Simulates a random walk of length "walk_length" starting from node "node"
-def random_walk(G: nx.Graph, source_node: int, walk_length: int):
+def random_walk(neighbors_dict: dict, source_node: Union[int, str], walk_length: int) -> List[Union[int, str]]:
+    """Slightly optimized random walk 
+    - using a precomputed dictionary structure to avoid using list -> remove redundant calls to list(G.neighbors(node))
+    - use randint instead of np.random.choice!
+
+    Returns:
+        List[int|str]: Random walk
+    """
     walk = [source_node]
     node = source_node
     for _ in range(walk_length-1):
-        neighors = list(G.neighbors(node))
+        neighors = neighbors_dict[node]
         degree = len(neighors)
-        degree, G.degree(node)-1
-        node = neighors[np.random.choice(degree)]
+        node = neighors[randint(0, degree-1)]
         walk.append(node)
     assert len(walk)==walk_length
     return walk
 
-
 ############## Task 2
 # Runs "num_walks" random walks from each node
 def generate_walks(G: nx.Graph, num_walks: int, walk_length: int):
+    # Precompute neighbors with a dictionary...
+    neighbors_dict = {node: list(G.neighbors(node)) for node in G.nodes()}
+
     walks = []
     for i in range(num_walks):
-        for node in G.nodes():
-            walks.append(random_walk(G, node, walk_length))
+        for node in G.nodes():    
+            walks.append(random_walk(neighbors_dict, node, walk_length))
     permuted_walks = np.random.permutation(walks)
     return permuted_walks.tolist()
 
