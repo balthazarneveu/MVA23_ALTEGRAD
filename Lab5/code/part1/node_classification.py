3b70fe3b365b33145f6c307f4822b0670c2b9f35
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-14 17:23:16 +0100

    task 5, 6, 7

--- Lab5/code/part1/node_classification.py
+++ Lab5/code/part1/node_classification.py
@@ -11,7 +11,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.manifold import SpectralEmbedding
 from sklearn.metrics import accuracy_score
 from deepwalk import deepwalk
-
+import matplotlib.pyplot as plt
 
 # Loads the karate network
 G = nx.read_weighted_edgelist('../data/karate.edgelist', delimiter=' ', nodetype=int, create_using=nx.Graph())
@@ -35,18 +35,15 @@ y = np.array(y)
 
 ############## Task 5
 # Visualizes the karate network
-
-##################
-# your code here #
-##################
-
+nx.draw_networkx(G, node_color=y)
+plt.show()
 
 ############## Task 6
 # Extracts a set of random walks from the karate network and feeds them to the Skipgram model
 n_dim = 128
 n_walks = 10
 walk_length = 20
-model = # your code here
+model = deepwalk(G, n_walks, walk_length, n_dim)
 
 embeddings = np.zeros((n, n_dim))
 for i, node in enumerate(G.nodes()):
@@ -65,12 +62,9 @@ y_test = y[idx_test]
 
 ############## Task 7
 # Trains a logistic regression classifier and use it to make predictions
-
-
-##################
-# your code here #
-##################
-
+clf = LogisticRegression(solver='lbfgs')
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
 
 ############## Task 8
 # Generates spectral embeddings
