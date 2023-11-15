8bd34fcff37104e9ccd281e13c576577b49bdfad
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-15 01:27:57 +0000

    fix node classification (task6)

--- Lab5/code/part1/node_classification.py
+++ Lab5/code/part1/node_classification.py
@@ -12,16 +12,17 @@ from sklearn.manifold import SpectralEmbedding
 from sklearn.metrics import accuracy_score
 from deepwalk import deepwalk
 import matplotlib.pyplot as plt
-
+from pathlib import Path
+DATA_ROOT = Path(__file__).parent
 # Loads the karate network
-G = nx.read_weighted_edgelist('../data/karate.edgelist', delimiter=' ', nodetype=int, create_using=nx.Graph())
+G = nx.read_weighted_edgelist(DATA_ROOT/'../data/karate.edgelist', delimiter=' ', nodetype=int, create_using=nx.Graph())
 print("Number of nodes:", G.number_of_nodes())
 print("Number of edges:", G.number_of_edges())
 
 n = G.number_of_nodes()
 
 # Loads the class labels
-class_labels = np.loadtxt('../data/karate_labels.txt', delimiter=',', dtype=np.int32)
+class_labels = np.loadtxt(DATA_ROOT/'../data/karate_labels.txt', delimiter=',', dtype=np.int32)
 idx_to_class_label = dict()
 for i in range(class_labels.shape[0]):
     idx_to_class_label[class_labels[i,0]] = class_labels[i,1]
@@ -46,12 +47,14 @@ walk_length = 20
 model = deepwalk(G, n_walks, walk_length, n_dim)
 
 embeddings = np.zeros((n, n_dim))
+nodes = model.wv.index_to_key[:n]
 for i, node in enumerate(G.nodes()):
-    embeddings[i,:] = model.wv[str(node)]
+    embeddings[i,:] = model.wv[node]
 
+TRAIN_RATIO = 0.8
 idx = np.random.RandomState(seed=42).permutation(n)
-idx_train = idx[:int(0.8*n)]
-idx_test = idx[int(0.8*n):]
+idx_train = idx[:int(TRAIN_RATIO*n)]
+idx_test = idx[int(TRAIN_RATIO*n):]
 
 X_train = embeddings[idx_train,:]
 X_test = embeddings[idx_test,:]
@@ -65,7 +68,8 @@ y_test = y[idx_test]
 clf = LogisticRegression(solver='lbfgs')
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
-
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
 ############## Task 8
 # Generates spectral embeddings
 
