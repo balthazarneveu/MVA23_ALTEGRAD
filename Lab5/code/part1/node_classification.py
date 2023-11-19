8c1893c4961d70a2e258154b2bf72d712a567650
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-19 11:43:58 +0100

    task 8 classifier comparisons

--- Lab5/code/part1/node_classification.py
+++ Lab5/code/part1/node_classification.py
@@ -13,7 +13,7 @@ from sklearn.metrics import accuracy_score
 from deepwalk import deepwalk
 import matplotlib.pyplot as plt
 from pathlib import Path
-from typing import Tuple
+from typing import Tuple, Dict
 DATA_ROOT = Path(__file__).parent/".."/"data"
 
 
@@ -86,7 +86,9 @@ def shuffle_split_dataset(
         embeddings: np.ndarray,
         labels:np.ndarray,
         number_of_nodes,
-        train_ratio = 0.8,seed=42) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        train_ratio = 0.8,
+        seed=42
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     idx = np.random.RandomState(seed=seed).permutation(number_of_nodes)
     idx_train = idx[:int(train_ratio*number_of_nodes)]
     idx_test = idx[int(train_ratio*number_of_nodes):]
@@ -118,16 +120,44 @@ def compute_rw_laplacian(graph: nx.Graph) -> np.ndarray:
     laplacian = eye(adj.shape[0])- diags(d).dot(adj) # keeps the spasity!
     return laplacian
 
+def spectral_embeddings(graph: nx.Graph, k=2) -> np.ndarray:
+    laplacian = compute_rw_laplacian(graph)
+    sorted_eigen_values, eigen_vectors = eigs(
+        laplacian,
+        which="SR", # SR for smallest real part
+        k=k
+    )
+    sorted_eigen_values = sorted_eigen_values.real
+    u_matrix = eigen_vectors.real # matrix of k sorted eigen vector
+    print(u_matrix.shape)
+    return u_matrix
 
 
-def study_classifier(G: nx.Graph, emb:np.ndarray, labels: np.ndarray):
-    train_ratios = np.linspace(0.1, 0.9, 30)
-    accuracies = np.zeros_like(train_ratios)
-    for idx, train_ratio in enumerate(train_ratios):
-        X_train, y_train, X_test, y_test = shuffle_split_dataset(
-            emb, labels, G.number_of_nodes(), train_ratio=train_ratio)
-        accuracies[idx] = classification(X_train, y_train, X_test, y_test)
-    plt.plot(train_ratios, accuracies, "g-+", label="Deepwalk embeddings")
+def study_classifiers(G: nx.Graph, embedding_list:Dict[str, np.ndarray], labels: np.ndarray, n_runs=10):
+    train_ratios = np.linspace(0.2, 0.9, 30)
+    plt.figure(figsize=(5, 5))
+    for embed_type_index, (name, embedding) in enumerate(embedding_list.items()):
+        color = "gcr"[embed_type_index]
+        accuracies = np.zeros_like(train_ratios)
+        std_dev = np.zeros_like(train_ratios)
+        mini, maxi = np.zeros_like(train_ratios), np.zeros_like(train_ratios)
+        for idx, train_ratio in enumerate(train_ratios):
+            current_accuracies = np.zeros(n_runs)
+            for idx_seed, seed in enumerate(range(42, 42+n_runs)):
+                X_train, y_train, X_test, y_test = shuffle_split_dataset(
+                    embedding, labels,
+                    G.number_of_nodes(),
+                    train_ratio=train_ratio,
+                    seed=seed
+                )
+                current_accuracies[idx_seed] = classification(X_train, y_train, X_test, y_test)
+            accuracies[idx] = np.mean(current_accuracies)
+            std_dev[idx] = np.std(current_accuracies)
+            mini[idx], maxi[idx] = np.min(current_accuracies), np.max(current_accuracies)
+            plt.plot([train_ratio, train_ratio], [mini[idx], maxi[idx]], "-^", alpha=0.2, color=color)
+        plt.plot(train_ratios, accuracies, "-o", color=color, label=name, linewidth=5)
+        
+        # plt.errorbar(train_ratios, accuracies, std_dev, linestyle='None', marker='^')
     plt.xlim(0, 1.)
     plt.ylim(0, 1.01)
     plt.xlabel("Training ratio")
@@ -138,10 +168,18 @@ def study_classifier(G: nx.Graph, emb:np.ndarray, labels: np.ndarray):
 
 def main(viz=True):
     G, y_true = load_labelled_graph()
+    emb_lap = spectral_embeddings(G)
     if viz:
         visualize_network(G, y_true, title="Groundtruth")
-    emb = deepwalk_embeddings(G)
-    study_classifier(G, emb, y_true)
+    emb_dw = deepwalk_embeddings(G)
+    study_classifiers(
+        G,
+        {
+            "Deepwalk embeddings": emb_dw,
+            "Laplacian embeddings": emb_lap,
+        },
+        y_true
+    )
     
 
 if __name__ == "__main__":
