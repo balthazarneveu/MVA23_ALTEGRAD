eb80482126046c73b38071fee7fd7dee808578da
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-21 23:31:10 +0100

    task 2 doc

--- Lab6/code/part1/models.py
+++ Lab6/code/part1/models.py
@@ -8,12 +8,12 @@ import torch.nn.functional as F
 
 
 class GNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim_1, hidden_dim_2, hidden_dim_3, n_class):
+    def __init__(self, input_dim: int, hidden_dim_1: int, hidden_dim_2: int, hidden_dim_3: int, n_class: int):
         super(GNN, self).__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim_1)  # W0
-        self.fc2 = nn.Linear(hidden_dim_1, hidden_dim_2)  # W1
-        self.fc3 = nn.Linear(hidden_dim_2, hidden_dim_3)
-        self.fc4 = nn.Linear(hidden_dim_3, n_class)
+        self.fc1 = nn.Linear(input_dim, hidden_dim_1)     # W0 (d,  h1)
+        self.fc2 = nn.Linear(hidden_dim_1, hidden_dim_2)  # W1 (h1, h2)
+        self.fc3 = nn.Linear(hidden_dim_2, hidden_dim_3)  # W2 (h2, h3)
+        self.fc4 = nn.Linear(hidden_dim_3, n_class)       # W3 (h3, nclass)
         self.relu = nn.ReLU()
 
     def forward(self, x_in: torch.Tensor, adj: torch.Tensor, idx: torch.Tensor) -> torch.Tensor:
@@ -74,14 +74,17 @@ class GNN(nn.Module):
             torch.Tensor: [N] logits after softmax
         """
         # GCN LAYERS
+        # 2 x {Fully connected projection -> Message passing using the adjacency matrix -> Relu}
         z0 = self.relu(torch.mm(adj, self.fc1(x_in)))
         z1 = self.relu(torch.mm(adj, self.fc2(z0)))
         # READOUT
+        # Squeeze the graph "spatial" dimension into a single [N, h2]
         idx = idx.unsqueeze(1).repeat(1, z1.size(1))
         out = torch.zeros(int(torch.max(idx)+1), z1.size(1), device=x_in.device)
         out = out.scatter_add_(0, idx, z1)  # readout
 
         # CLASSIFICATION LAYERS
+        # Fully connected projection -> Relu -> Fully connected projection
         out = self.relu(self.fc3(out))
         out = self.fc4(out)
         return F.log_softmax(out, dim=1)
