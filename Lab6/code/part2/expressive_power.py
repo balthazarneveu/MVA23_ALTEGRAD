74a3d926847de71294725e314eb88175f7722597
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-21 14:27:20 +0100

    intitate lab6

---
+++ Lab6/code/part2/expressive_power.py
@@ -0,0 +1,73 @@
+"""
+Deep Learning on Graphs - ALTEGRAD - Nov 2023
+"""
+
+import networkx as nx
+import numpy as np
+import scipy.sparse as sp
+import torch
+
+from models import GNN
+from utils import sparse_mx_to_torch_sparse_tensor
+
+# Initializes device
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+# Hyperparameters
+hidden_dim = 32
+output_dim = 4
+dropout = 0.0
+neighbor_aggr = 'mean'
+readout = 'mean'
+
+
+############## Task 4
+        
+##################
+# your code here #
+##################
+
+
+
+
+############## Task 5
+        
+##################
+# your code here #
+##################
+
+
+
+
+############## Task 8
+        
+##################
+# your code here #
+##################
+
+
+
+
+############## Task 9
+        
+##################
+# your code here #
+##################
+
+
+
+
+############## Task 10
+        
+##################
+# your code here #
+##################
+
+
+
+
+############## Task 11
+        
+##################
+# your code here #
+##################
