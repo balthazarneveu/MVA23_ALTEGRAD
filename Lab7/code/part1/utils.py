736eb413c7160575c3cf056447b5541ff6b2603f
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-28 15:40:15 +0100

    initiate lab 7

---
+++ Lab7/code/part1/utils.py
@@ -0,0 +1,29 @@
+"""
+Learning on Sets and Graph Generative Models - ALTEGRAD - Nov 2023
+"""
+
+import numpy as np
+
+
+def create_train_dataset():
+    n_train = 100000
+    max_train_card = 10
+
+    ############## Task 1
+    
+    ##################
+    # your code here #
+    ##################
+
+    return X_train, y_train
+
+
+def create_test_dataset():
+    
+    ############## Task 2
+    
+    ##################
+    # your code here #
+    ##################
+
+    return X_test, y_test
