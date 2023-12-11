b88cce6b808c886462dda661e78dd9f452ee315a
Balthazar Neveu <balthazarneveu@gmail.com>
2023-12-11 16:44:10 +0100

    compare accuracy across epochs

--- Lab7/code/part1/eval.py
+++ Lab7/code/part1/eval.py
@@ -4,18 +4,37 @@ Learning on Sets and Graph Generative Models - ALTEGRAD - Nov 2023
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, mean_absolute_error
 import torch
-from train import DEEPSET, LSTMSET, RESULTS_ROOT, training_loop
+from train import DEEPSET, LSTMSET, RESULTS_ROOT
 from train import get_results_path
 import logging
 from utils import create_test_dataset
 from models import DeepSets, LSTM
+import numpy as np
+
+
+def get_checkpoint(saved_models_path):
+    logging.info("Loading DeepSets checkpoint!")
+    checkpoint_deepset = torch.load(saved_models_path[DEEPSET])
+    logging.info("Loading LSTM checkpoint!")
+    checkpoint_lstm = torch.load(saved_models_path[LSTMSET])
+    return checkpoint_deepset["state_dict"], checkpoint_lstm["state_dict"]
+
+
+def generate_test_set():
+    # Generates test data
+    X_test, y_test, cards = create_test_dataset(step_test_card=5)
+    cards = [X_test[i].shape[1] for i in range(len(X_test))]
+    n_samples_per_card = X_test[0].shape[0]
+    n_digits = 11
+    return X_test, y_test, cards, n_samples_per_card, n_digits
 
 
 def evaluate(results_path=RESULTS_ROOT, **kwargs):
     saved_models_path = get_results_path(results_path)
-    if not saved_models_path[DEEPSET].exists() or not saved_models_path[LSTMSET].exists():
-        logging.warning("No model found, training models...")
-        training_loop(results_path, **kwargs)
+    # saved_models_path = get_results_path(results_path, name=f"{epoch}")
+    # if not saved_models_path[DEEPSET].exists() or not saved_models_path[LSTMSET].exists():
+    #     logging.warning("No model found, training models...")
+    #     training_loop(results_path, **kwargs)
     # Initializes device
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
@@ -24,24 +43,17 @@ def evaluate(results_path=RESULTS_ROOT, **kwargs):
     embedding_dim = 128
     hidden_dim = 64
 
-    # Generates test data
-    X_test, y_test, cards = create_test_dataset(step_test_card=5)
-    cards = [X_test[i].shape[1] for i in range(len(X_test))]
-    n_samples_per_card = X_test[0].shape[0]
-    n_digits = 11
-
+    X_test, y_test, cards, n_samples_per_card, n_digits = generate_test_set()
+    checkpoint_deepset, checkpoint_lstm = get_checkpoint(saved_models_path)
     # Retrieves DeepSets model
     deepsets = DeepSets(n_digits, embedding_dim, hidden_dim).to(device)
-    logging.info("Loading DeepSets checkpoint!")
-    checkpoint = torch.load(saved_models_path[DEEPSET])
-    deepsets.load_state_dict(checkpoint['state_dict'])
+
+    deepsets.load_state_dict(checkpoint_deepset)
     deepsets.eval()
 
     # Retrieves LSTM model
     lstm = LSTM(n_digits, embedding_dim, hidden_dim).to(device)
-    logging.info("Loading LSTM checkpoint!")
-    checkpoint = torch.load(saved_models_path[LSTMSET])
-    lstm.load_state_dict(checkpoint['state_dict'])
+    lstm.load_state_dict(checkpoint_lstm)
     lstm.eval()
 
     # Load 2 models at once, evaluate them on the same sets
@@ -60,10 +72,10 @@ def evaluate(results_path=RESULTS_ROOT, **kwargs):
                 y_pred_deepsets.append(deepsets(x_batch))
                 y_pred_lstm.append(lstm(x_batch))
             y_pred_deepsets = torch.cat(y_pred_deepsets)
-            y_pred_deepsets = torch.round(y_pred_deepsets)
+            y_pred_deepsets = y_pred_deepsets
             y_pred_deepsets = y_pred_deepsets.detach().cpu().numpy()
 
-            acc_deepsets = accuracy_score(y_test[i], y_pred_deepsets)
+            acc_deepsets = accuracy_score(y_test[i], np.round(y_pred_deepsets))
             mae_deepsets = mean_absolute_error(y_test[i], y_pred_deepsets)
 
             results[DEEPSET]['acc'].append(acc_deepsets)
@@ -74,7 +86,7 @@ def evaluate(results_path=RESULTS_ROOT, **kwargs):
             y_pred_lstm = torch.round(y_pred_lstm)
             y_pred_lstm = y_pred_lstm.detach().cpu().numpy()
 
-            acc_lstm = accuracy_score(y_test[i], y_pred_lstm)
+            acc_lstm = accuracy_score(y_test[i], np.round(y_pred_lstm))
             mae_lstm = mean_absolute_error(y_test[i], y_pred_lstm)
             results[LSTMSET]['acc'].append(acc_lstm)
             results[LSTMSET]['mae'].append(mae_lstm)
@@ -82,23 +94,37 @@ def evaluate(results_path=RESULTS_ROOT, **kwargs):
     return results
 
 
-def plot_results(results_dict):
+def plot_results(results_dict, extra_tile=""):
     # Task 7
     plt.figure(figsize=(10, 5))
     plt.grid()
-    for model_name, results in results_dict.items():
-        plt.plot(results["cardinals"], results[DEEPSET]['acc'], "-o", alpha=results["epoch"]/8., label=f'{model_name} DeepSets')
-        plt.plot(results["cardinals"], results[LSTMSET]['acc'], "--o", alpha=results["epoch"]/8., label=f'{model_name} LSTM')
-    plt.legend()
-    plt.xlabel("Maximum cardinality of the sets")
-    plt.ylabel("Accuracy")
-    plt.show()
+    for metric, metric_pretty_name in [("acc", "accuracy"), ("mae", "MAE")]:
+        for model_name, results in results_dict.items():
+            label = f'{model_name}'
+            # MAE={results[DEEPSET]["mae"]
+            # {results[LSTMSET]["mae"]}
+            plt.plot(results["cardinals"], results[DEEPSET][metric], "-o",
+                     alpha=results["epoch"]/19.,
+                     #  color="green",
+                     label=None if label is None else (label + f' DeepSets'))
+            plt.plot(results["cardinals"], results[LSTMSET][metric], "--o",
+                     alpha=results["epoch"]/19.,
+                     color="cyan",
+                     label=None if label is None else (label + f' LSTM '))
+        plt.title(f"Evolution of the {metric_pretty_name} with the cardinality of the sets"+extra_tile)
+        plt.grid()
+        plt.legend(loc='lower right')
+        plt.xlabel("Maximum cardinality of the sets")
+        plt.ylabel(metric_pretty_name)
+        plt.show()
 
 
 if __name__ == "__main__":
     all_results = {}
-    for n_epoch in range(3, 8):
-        results = evaluate(RESULTS_ROOT/f"n={n_epoch}", epochs=n_epoch)
+    selection = range(20)
+    # selection = [19]
+    for n_epoch in selection:
+        results = evaluate(RESULTS_ROOT/"seed=42"/f"{n_epoch}")
         results["epoch"] = n_epoch
-        all_results[f"{n_epoch} epochs"] = results
+        all_results[f"{n_epoch+1} epochs"] = results
     plot_results(all_results)
--- Lab7/code/part1/train.py
+++ Lab7/code/part1/train.py
@@ -10,11 +10,14 @@ import torch.nn as nn
 from pathlib import Path
 from utils import create_train_dataset
 from models import DeepSets, LSTM
+from typing import Optional
 DEEPSET, LSTMSET = "deepset", "lstm"
 RESULTS_ROOT = Path('__results')
 
 
-def get_results_path(results_path=RESULTS_ROOT) -> dict:
+def get_results_path(results_path=RESULTS_ROOT, name: Optional[str] = None) -> dict:
+    if name is not None:
+        results_path = results_path/name
     results_path.mkdir(parents=True, exist_ok=True)
     saved_models_path = {
         DEEPSET: results_path/'model_deepsets.pth.tar',
@@ -29,9 +32,10 @@ def training_loop(
     batch_size=64,
     embedding_dim=128,
     hidden_dim=64,
-    learning_rate=0.001
+    learning_rate=0.001,
+    seed=42
 ) -> dict:
-    saved_models_path = get_results_path(results_path)
+    torch.manual_seed(seed)
     # Initializes device
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
@@ -47,88 +51,90 @@ def training_loop(
     optimizer = optim.Adam(deepsets.parameters(), lr=learning_rate)
     loss_function = nn.L1Loss()
 
-    if not saved_models_path[DEEPSET].exists():
-        # Trains the DeepSets model
-        for epoch in range(epochs):
-            t = time.time()
-            deepsets.train()
+    train_dict_deepset = dict()
+    # Trains the DeepSets model
+    for epoch in range(epochs):
+        t = time.time()
+        deepsets.train()
 
-            train_loss = 0
-            count = 0
-            permut = np.random.permutation(n_train)
-            for i in range(0, n_train, batch_size):
+        train_loss = 0
+        count = 0
+        permut = np.random.permutation(n_train)
+        for i in range(0, n_train, batch_size):
 
-                # Task 5
-                optimizer.zero_grad()
-                permut_indexes = permut[i:min(i+batch_size, n_train-1)]
-                x_batch = torch.Tensor(X_train[permut_indexes, :]).to(device, dtype=torch.int64)
-                y_batch = torch.Tensor(y_train[permut_indexes]).to(device, dtype=torch.int64)
-                output = deepsets(x_batch)
-                loss = loss_function(output, y_batch)
-                loss.backward()
-                optimizer.step()
-                train_loss += loss.item() * output.size(0)
-                count += output.size(0)
+            # Task 5
+            optimizer.zero_grad()
+            permut_indexes = permut[i:min(i+batch_size, n_train-1)]
+            x_batch = torch.Tensor(X_train[permut_indexes, :]).to(device, dtype=torch.int64)
+            y_batch = torch.Tensor(y_train[permut_indexes]).to(device, dtype=torch.int64)
+            output = deepsets(x_batch)
+            loss = loss_function(output, y_batch)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item() * output.size(0)
+            count += output.size(0)
 
-            print('Epoch: {:04d}'.format(epoch+1),
-                  'loss_train: {:.4f}'.format(train_loss / count),
-                  'time: {:.4f}s'.format(time.time() - t))
+        print('Epoch: {:04d}'.format(epoch+1),
+              'loss_train: {:.4f}'.format(train_loss / count),
+              'time: {:.4f}s'.format(time.time() - t))
 
         # Stores DeepSets model into disk
         train_dict_deepset = {
+            'epoch': epoch,
             'state_dict': deepsets.state_dict(),
             'optimizer': optimizer.state_dict(),
         }
+        # Save at all epoch to check convergence in eval
+        saved_models_path = get_results_path(results_path, name=f"{epoch}")
         torch.save(train_dict_deepset, saved_models_path[DEEPSET])
 
-        print("Finished training for DeepSets model")
-    else:
-        train_dict_deepset = torch.load(saved_models_path[DEEPSET])
+    print("Finished training for DeepSets model")
 
+    torch.manual_seed(seed)
     # Initializes LSTM model and optimizer
     lstm = LSTM(n_digits, embedding_dim, hidden_dim).to(device)
     optimizer = optim.Adam(lstm.parameters(), lr=learning_rate)
     loss_function = nn.L1Loss()
-    if not saved_models_path[LSTMSET].exists():
-        # Trains the LSTM model
-        for epoch in range(epochs):
-            t = time.time()
-            lstm.train()
+    train_dict_lstm = {}
 
-            train_loss = 0
-            count = 0
-            permut = np.random.permutation(n_train)
-            for i in range(0, n_train, batch_size):
+    # Trains the LSTM model
+    for epoch in range(epochs):
+        t = time.time()
+        lstm.train()
 
-                # Task 5
-                permut_indexes = permut[i:min(i+batch_size, n_train-1)]
-                x_batch = torch.Tensor(X_train[permut_indexes, :]).to(device, dtype=torch.int64)
-                y_batch = torch.Tensor(y_train[permut_indexes]).to(device)
+        train_loss = 0
+        count = 0
+        permut = np.random.permutation(n_train)
+        for i in range(0, n_train, batch_size):
 
-                optimizer.zero_grad()
-                output = lstm(x_batch)
-                loss = loss_function(output, y_batch)
-                loss.backward()
-                optimizer.step()
-                train_loss += loss.item() * output.size(0)
-                count += output.size(0)
+            # Task 5
+            permut_indexes = permut[i:min(i+batch_size, n_train-1)]
+            x_batch = torch.Tensor(X_train[permut_indexes, :]).to(device, dtype=torch.int64)
+            y_batch = torch.Tensor(y_train[permut_indexes]).to(device)
 
-            print('Epoch: {:04d}'.format(epoch+1),
-                  'loss_train: {:.4f}'.format(train_loss / count),
-                  'time: {:.4f}s'.format(time.time() - t))
+            optimizer.zero_grad()
+            output = lstm(x_batch)
+            loss = loss_function(output, y_batch)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item() * output.size(0)
+            count += output.size(0)
+
+        print('Epoch: {:04d}'.format(epoch+1),
+              'loss_train: {:.4f}'.format(train_loss / count),
+              'time: {:.4f}s'.format(time.time() - t))
 
         # Stores LSTM model into disk
         train_dict_lstm = {
             'state_dict': lstm.state_dict(),
             'optimizer': optimizer.state_dict(),
         }
+        saved_models_path = get_results_path(results_path, name=f"{epoch}")
         torch.save(train_dict_lstm, saved_models_path[LSTMSET])
+    print("Finished training for LSTM model")
 
-        print("Finished training for LSTM model")
-    else:
-        train_dict_lstm = torch.load(saved_models_path[DEEPSET])
     return {DEEPSET: train_dict_lstm, LSTMSET: train_dict_lstm}
 
 
 if __name__ == '__main__':
-    training_loop()
+    training_loop(RESULTS_ROOT/"seed=42", seed=42)
