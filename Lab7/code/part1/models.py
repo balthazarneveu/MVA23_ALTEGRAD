5b38fd06fa8ecbf4656159a55a96f2f47e752248
Balthazar Neveu <balthazarneveu@gmail.com>
2023-12-11 14:03:30 +0100

    document Tasks 3 and 4

--- Lab7/code/part1/models.py
+++ Lab7/code/part1/models.py
@@ -54,34 +54,72 @@ class DeepSets(nn.Module):
         Task 3
         ======
         ```text
+        Implement the DeepSets architecture
+        - an embedding layer which projects each digit to a latent space of dimensionality h1
+        - a fully-connected layer with h2 hidden units followed by a tanh activation function
+        - a sum aggregator which computes the sum of the elements of each set
+        - a fully-connected layer with 1 unit since the output of the model needs to be a scalar
+        (i.e., the prediction of the sum of the digits contained in the set)
+        ```
+
+        Implementation details
+        ======================
         - x as a vector [N, L] with values
         - Embed the integers  -> [N, L, embedding_dim]
         - FC1: [N, L, embedding_dim] -> [N, L, hidden_dim]
         - Non linearity (tanh) + Sum pooling: [N, L, hidden_dim] -> [N, hidden_dim]
         - FC2: [N, hidden_dim] -> [N, 1]
-        ```
+
         """
 
         # Task 3
         x = self.embedding(x)  # [N, L] -> [N, L, embedding_dim]
         x = self.tanh(self.fc1(x))  # FC1+Non linearity -> [N, L, hidden_dim]
-        x = torch.sum(x, dim=1)  # Sum pooling -> [N, L]
+        x = torch.sum(x, dim=1)  # pooling=sum -> [N, L]
         x = self.fc2(x)  # FC2 -> [N, 1]
         return x.squeeze()
 
 
 class LSTM(nn.Module):
-    def __init__(self, input_dim, embedding_dim, hidden_dim):
+    def __init__(self, input_dim: int, embedding_dim: int, hidden_dim: int, output_dim: int = 1):
         super(LSTM, self).__init__()
 
         self.embedding = nn.Embedding(input_dim, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, 1)
+        # num_layers=1, bidirectional=False
+        self.fc = nn.Linear(hidden_dim, output_dim)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """LSTM on the "set" x
+
+        Args:
+            x (torch.Tensor): [N, L] (batch first)
+
+        Returns:
+            torch.Tensor: prediction [N]
+        Task 4:
+        =======
+
+        ```text
+        Implement the LSTM architecture
+        - an embedding layer which projects each digit
+        to a latent space of dimensionality h1
+        - an LSTM layer with h2 hidden units
+        - a fully-connected layer which takes the LSTM hidden state output
+        for the last time step as input and outputs a scalar (out_dim=1)
+        ```
+        h1 = embedding_dim
+        h2 = hidden_dim
+
+        Note on LSTM use:
+        =================
+        extract x = all hidden states for the last iteration (last word)
+        Here num_layers=1 and bidirectional=False -> 1*1*hidden_dim
+        https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html
+        """
         # Task 4
-        x = self.embedding(x)
-        _, (x, _) = self.lstm(x)
-        x = self.fc(x)  # extract all hidden states
+        x = self.embedding(x)  # [N, L, embedding_dim]
+        _, x, _ = self.lstm(x)  # -> [N, 1*1*hidden_dim] (batch_first=True)
+        x = self.fc(x)  # [N, hidden_dim] -> [N, 1]
 
         return x.squeeze()
